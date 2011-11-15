fce5a07595c8fd1038c85a46c0a1383f793adeb3
goeran <mail@goeran.no>
2011-11-15 14:14:31 +0100

    removed an crappy test

--- ODiff.Tests/Learning/Learning_about_reflection.cs
+++ ODiff.Tests/Learning/Learning_about_reflection.cs
@@ -6,15 +6,5 @@ namespace ODiff.Tests.Learning
 {
     public class Learning_about_reflection
     {
-        [Test]
-        public void Is_a_x()
-        {
-            Assert.IsTrue(new CustomList().IsList());
-        }
-
-        private class CustomList : List<string>
-        {
-            
-        }
     }
 }
