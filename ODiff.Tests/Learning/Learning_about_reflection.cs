b50d0d59c2870544741e794d41ba2f550a73455f
goeran <mail@goeran.no>
2011-11-23 13:29:12 +0100

    moved reflection learning tests to the right file

--- ODiff.Tests/Learning/Learning_about_reflection.cs
+++ ODiff.Tests/Learning/Learning_about_reflection.cs
@@ -1,10 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using NUnit.Framework;
-using ODiff.Extensions;
+using ODiff.Tests.Fakes;
 
 namespace ODiff.Tests.Learning
 {
     public class Learning_about_reflection
     {
+        [TestFixture]
+        public class Reflection_and_enums
+        {
+            [Test]
+            public void How_to_figure_out_if_an_value_is_an_enum()
+            {
+                Object gender = Gender.Femal;
+
+                Assert.IsTrue(gender.GetType().IsEnum);
+
+            }
+        }
+
+        [TestFixture]
+        public class Enums_and_boxing
+        {
+            [Test]
+            public void Compare_enums_as_objects()
+            {
+                Object male = Gender.Male;
+                Object anotherMaleGender = Gender.Male;
+
+                Assert.IsFalse(male == anotherMaleGender);
+                Assert.IsTrue(male.Equals(anotherMaleGender));
+            }
+        }
     }
 }
