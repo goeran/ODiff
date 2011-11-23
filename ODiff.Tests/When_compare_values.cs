2e7546bbd2d7ee7ee0a85788dbc6b6e4d161852c
goeran <mail@goeran.no>
2011-11-23 13:34:47 +0100

    started to write tests on comparing simple values - not graphs

--- ODiff.Tests/When_compare_values.cs
+++ ODiff.Tests/When_compare_values.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NUnit.Framework;
 
 namespace ODiff.Tests
 {
     class When_compare_values
     {
+        [TestFixture]
+        public class When_compare_with_nulls
+        {
+            [Test]
+            public void It_will_report_diff_if_one_value_is_null()
+            {
+                Assert.IsTrue(Diff.ObjectValues(1, null).DiffFound);
+                Assert.IsTrue(Diff.ObjectValues(null, 1).DiffFound);
+            }
+        }
+
     }
 }
