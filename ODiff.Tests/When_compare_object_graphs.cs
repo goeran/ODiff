7f87eeac08a0395fcdd93e3200e0f055779baec9
goeran <mail@goeran.no>
2011-12-01 06:51:56 +0100

    added tests for comparing structs

--- ODiff.Tests/When_compare_object_graphs.cs
+++ ODiff.Tests/When_compare_object_graphs.cs
@@ -396,6 +396,40 @@ namespace ODiff.Tests
         }
 
         [TestFixture]
+        public class When_diff_structs
+        {
+            [Test]
+            public void It_will_detect_diff_on_public_properties()
+            {
+                var left = new Employee { Name = "Steve" };
+                var right = new Employee();
+
+                var diff = Diff.ObjectValues(left, right);
+
+                Assert.AreEqual(1, diff.Table.Rows.Count());
+                Assert.AreEqual("Name", diff.Table[0].MemberPath);
+            }
+
+            [Test]
+            public void It_will_detect_diff_on_public_fields()
+            {
+                var left = new Employee { Number = 1 };
+                var right = new Employee { Number = 2 };
+
+                var diff = Diff.ObjectValues(left, right);
+
+                Assert.AreEqual(1, diff.Table.Rows.Count());
+                Assert.AreEqual("Number", diff.Table[0].MemberPath);
+            }
+
+            struct Employee
+            {
+                public int Number;
+                public string Name { get; set; }
+            }
+        }
+
+        [TestFixture]
         public class When_intercept_nodes
         {
             [Test]
