11b86c0d3f8fcc51af33180d1fd840f4cc4a3343
goeran <mail@goeran.no>
2012-08-09 14:30:41 +0200

    Cross diffing in all of the tests, to make sure the diff give same result for both sides of the diff

--- ODiff.Tests/Diff_lists.cs
+++ ODiff.Tests/Diff_lists.cs
@@ -15,55 +15,67 @@ namespace ODiff.Tests
             [Test]
             public void It_will_report_diff_on_length()
             {
-                var left = new List<string> { "a" };
-                var right = new List<string> { "a", "b" };
+                var listA = new List<string> { "a" };
+                var listB = new List<string> { "a", "b" };
 
-                var report = Diff.ObjectValues(left, right);
+                Cross.diff(listA, listB, (left, right) =>
+                {
+                    var report = Diff.ObjectValues(listA, listB);
+                    Console.WriteLine(report.Print());
 
-                Assert.IsTrue(report.DiffFound);
-                Assert.AreEqual(2, report.Table.Rows.Count());
-                Assert.AreEqual("Count", report.Table[1].MemberPath);
-                Assert.AreEqual(1, report.Table[1].LeftValue);
-                Assert.AreEqual(2, report.Table[1].RightValue);
+                    Assert.IsTrue(report.DiffFound);
+                    Assert.AreEqual(2, report.Table.Rows.Count());
+                    Assert.AreEqual("[1]", report.Table[0].MemberPath);
+                    Assert.AreEqual("Count", report.Table[1].MemberPath);
+                });
             }
 
             [Test]
             public void It_will_report_diff_on_content_in_lists()
             {
-                var left = new List<string> { "a", "a" };
-                var right = new List<string> { "a", "b" };
+                var listA = new List<string> { "a", "a" };
+                var listB = new List<string> { "a", "b" };
 
-                var report = Diff.ObjectValues(left, right);
+                Cross.diff(listA, listB, (left, right) =>
+                {
+                    var report = Diff.ObjectValues(left, right);
 
-                Assert.IsTrue(report.DiffFound);
-                Assert.AreEqual(1, report.Table.Rows.Count());
-                Assert.AreEqual("[1]", report.Table[0].MemberPath);
+                    Assert.IsTrue(report.DiffFound);
+                    Assert.AreEqual(1, report.Table.Rows.Count());
+                    Assert.AreEqual("[1]", report.Table[0].MemberPath);
+                });
             }
 
             [Test]
             public void It_will_report_diff_on_objects_in_lists()
             {
-                var left = new List<Person> { new Person { NameProperty = "Gøran" } };
-                var right = new List<Person> { new Person { NameProperty = "Gøran Hansen" } };
+                var listA = new List<Person> { new Person { NameProperty = "Gøran" } };
+                var listB = new List<Person> { new Person { NameProperty = "Gøran Hansen" } };
 
-                var report = Diff.ObjectValues(left, right);
+                Cross.diff(listA, listB, (left, right) =>
+                {
+                    var report = Diff.ObjectValues(left, right);
 
-                Assert.IsTrue(report.DiffFound);
-                Assert.AreEqual(1, report.Table.Rows.Count());
-                Assert.AreEqual("[0].NameProperty", report.Table[0].MemberPath);
+                    Assert.IsTrue(report.DiffFound);
+                    Assert.AreEqual(1, report.Table.Rows.Count());
+                    Assert.AreEqual("[0].NameProperty", report.Table[0].MemberPath);
+                });
             }
 
             [Test]
             public void It_will_report_diff_on_lists_in_lists()
             {
-                var left = new List<List<string>> { new List<string> { "a" } };
-                var right = new List<List<string>> { new List<string> { "a", "b" } };
+                var listA = new List<List<string>> { new List<string> { "a" } };
+                var listB = new List<List<string>> { new List<string> { "a", "b" } };
 
-                var report = Diff.ObjectValues(left, right);
+                Cross.diff(listA, listB, (left, righ) =>
+                {
+                    var report = Diff.ObjectValues(left, righ);
 
-                Assert.AreEqual(2, report.Table.Rows.Count());
-                Assert.AreEqual("[0][1]", report.Table[0].MemberPath);
-                Assert.AreEqual("[0].Count", report.Table[1].MemberPath);
+                    Assert.AreEqual(2, report.Table.Rows.Count());
+                    Assert.AreEqual("[0][1]", report.Table[0].MemberPath);
+                    Assert.AreEqual("[0].Count", report.Table[1].MemberPath);
+                });
             }
         }
 
@@ -74,25 +86,28 @@ namespace ODiff.Tests
             public void It_will_recursively_compare_all_members_of_list()
             {
                 var steve = FakeData.KnownPersons.SteveJobs;
-                var steveCopy = (Person)ObjectCloner.Clone(steve);
-                steveCopy.Children.RemoveAt(0);
+                var clonedSteve = (Person)ObjectCloner.Clone(steve);
+                clonedSteve.Children.RemoveAt(0);
 
-                var diff = Diff.ObjectValues(steve, steveCopy);
-                Console.WriteLine(diff.Print());
+                Cross.diff(steve, clonedSteve, (left, right) =>
+                {
+                    var diff = Diff.ObjectValues(left, right);
+                    Console.WriteLine(diff.Print());
 
-                Assert.IsTrue(diff.DiffFound);
-                Assert.AreEqual(11, diff.Table.Rows.Count());
-                Assert.AreEqual("Children[0].NameField", diff.Table[0].MemberPath);
-                Assert.AreEqual("Children[0].AgeField", diff.Table[1].MemberPath);
-                Assert.AreEqual("Children[0].NameProperty", diff.Table[2].MemberPath);
-                Assert.AreEqual("Children[0].AgeProperty", diff.Table[3].MemberPath);
-                Assert.AreEqual("Children[1].NameField", diff.Table[4].MemberPath);
-                Assert.AreEqual("Children[1].AgeField", diff.Table[5].MemberPath);
-                Assert.AreEqual("Children[1].NameProperty", diff.Table[6].MemberPath);
-                Assert.AreEqual("Children[1].AgeProperty", diff.Table[7].MemberPath);
-                Assert.AreEqual("Children[1].Children", diff.Table[8].MemberPath);
-                Assert.AreEqual("Children[2]", diff.Table[9].MemberPath);
-                Assert.AreEqual("Children.Count", diff.Table[10].MemberPath);
+                    Assert.IsTrue(diff.DiffFound);
+                    Assert.AreEqual(11, diff.Table.Rows.Count());
+                    Assert.AreEqual("Children[0].NameField", diff.Table[0].MemberPath);
+                    Assert.AreEqual("Children[0].AgeField", diff.Table[1].MemberPath);
+                    Assert.AreEqual("Children[0].NameProperty", diff.Table[2].MemberPath);
+                    Assert.AreEqual("Children[0].AgeProperty", diff.Table[3].MemberPath);
+                    Assert.AreEqual("Children[1].NameField", diff.Table[4].MemberPath);
+                    Assert.AreEqual("Children[1].AgeField", diff.Table[5].MemberPath);
+                    Assert.AreEqual("Children[1].NameProperty", diff.Table[6].MemberPath);
+                    Assert.AreEqual("Children[1].AgeProperty", diff.Table[7].MemberPath);
+                    Assert.AreEqual("Children[1].Children", diff.Table[8].MemberPath);
+                    Assert.AreEqual("Children[2]", diff.Table[9].MemberPath);
+                    Assert.AreEqual("Children.Count", diff.Table[10].MemberPath);
+                });
             }
 
             [Test]
@@ -105,10 +120,13 @@ namespace ODiff.Tests
                 listOfListsCopy.Last().RemoveAt(0);
                 listOfListsCopy.Last().Insert(0, "four edited");
 
-                var diff = Diff.ObjectValues(listOfLists, listOfListsCopy);
-                Assert.AreEqual("four edited", listOfListsCopy[1][0]);
-                Assert.AreEqual(1, diff.Table.Rows.Count());
-                Assert.AreEqual("[1][0]", diff.Table[0].MemberPath);
+                Cross.diff(listOfLists, listOfListsCopy, (left, right) =>
+                {
+                    var diff = Diff.ObjectValues(listOfLists, listOfListsCopy);
+                    Assert.AreEqual("four edited", listOfListsCopy[1][0]);
+                    Assert.AreEqual(1, diff.Table.Rows.Count());
+                    Assert.AreEqual("[1][0]", diff.Table[0].MemberPath);
+                });
             }
         }
 
@@ -119,29 +137,35 @@ namespace ODiff.Tests
             public void It_will_compare_all_members_in_list()
             {
                 var steve = FakeData.KnownPersons.SteveJobs;
-                var steveCopy = (Person)ObjectCloner.Clone(steve);
-                steveCopy.Tags.Add("phones");
+                var clonedSteve = (Person)ObjectCloner.Clone(steve);
+                clonedSteve.Tags.Add("phones");
 
-                var report = Diff.ObjectValues(steve, steveCopy);
+                Cross.diff(steve, clonedSteve, (left, right) =>
+                {
+                    var report = Diff.ObjectValues(left, right);
 
-                Assert.IsTrue(report.DiffFound);
-                Assert.AreEqual(2, report.Table.Rows.Count());
-                Assert.AreEqual("Tags[3]", report.Table[0].MemberPath);
-                Assert.AreEqual("Tags.Count", report.Table[1].MemberPath);
+                    Assert.IsTrue(report.DiffFound);
+                    Assert.AreEqual(2, report.Table.Rows.Count());
+                    Assert.AreEqual("Tags[3]", report.Table[0].MemberPath);
+                    Assert.AreEqual("Tags.Count", report.Table[1].MemberPath);
+                });
             }
 
             [Test]
             public void It_will_compare_all_members_in_enumerable_collection()
             {
                 var steve = FakeData.KnownPersons.SteveJobs;
-                var steveCopy = (Person)ObjectCloner.Clone(steve);
-                steveCopy.AddPhone("97122644");
+                var clonedSteve = (Person)ObjectCloner.Clone(steve);
+                clonedSteve.AddPhone("97122644");
 
-                var report = Diff.ObjectValues(steve, steveCopy);
+                Cross.diff(steve, clonedSteve, (left, right) =>
+                {
+                    var report = Diff.ObjectValues(left, right);
 
-                Assert.IsTrue(report.DiffFound);
-                Assert.AreEqual(1, report.Table.Rows.Count());
-                Assert.AreEqual("Phones[0]", report.Table[0].MemberPath);
+                    Assert.IsTrue(report.DiffFound);
+                    Assert.AreEqual(1, report.Table.Rows.Count());
+                    Assert.AreEqual("Phones[0]", report.Table[0].MemberPath);
+                });
             }
         }
     }
