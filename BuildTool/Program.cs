dd7bd5e3feba30f69b1f9782fbfcd49cf689a466
goeran <mail@goeran.no>
2013-08-23 13:39:44 +0200

    Made it easier to push new nuget packages using build script

---
+++ BuildTool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace BuildTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = args[0];
+            var version = args[1];
+            if (command.ToLower() == "create-nuspec")
+            {
+                using (var templateStream = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("BuildTool.Templates.ODiff.nuspec")))
+                {
+                    var template = templateStream.ReadToEnd();
+                    var output = template.Replace("$(version)", version);
+                    File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "ODiff.nuspec"), output);
+                }
+            }
+        }
+    }
+}
